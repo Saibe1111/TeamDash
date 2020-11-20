31a576ad8dd73a504a18244c55548283a6fef2ed
Marwan <marwan.mezroui.pro@gmail.com>
2020-11-20 23:13:10 +0100

    Update of index.php to take care of css, js files or pictures

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js|svg)$/', $_SERVER["REQUEST_URI"])) {  
+   return false;
+}
+
+//require_once('./bootstrap.php');
+
 require_once('./bootstrap.php');
