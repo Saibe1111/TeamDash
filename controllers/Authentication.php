3bba34052494d27e70728911560ba2b5f2c72f14
iTrixPro <marwan.mezroui.pro@gmail.com>
2020-12-14 04:40:00 +0100

    register operational

--- controllers/Authentication.php
+++ controllers/Authentication.php
@@ -2,13 +2,13 @@
 namespace Controllers;
 
 class Authentication extends \Controller {
-    
+
     public function login() {
         $this->render('login');
 
-        $this->loadModel("LoginManagement");
-
         if(!empty($_POST)) {
+            $this->loadModel("LoginManagement");
+
             if(!$this->LoginManagement->exist($_POST['mail'])) {
                 echo '<script> alert("Error: You have entered an invalid email address."); location.href="/";</script>';
                 exit();
@@ -27,6 +27,27 @@ class Authentication extends \Controller {
 
     public function register() {
         $this->render('register');
+
+        if (!empty($_POST)) {
+            $this->loadModel("RegisterManagement");
+            $user_data = [];
+            
+            list($firstname, $lastname) = explode(' ', $_POST['name']);
+            $user_data['firstname'] = htmlspecialchars($firstname);
+            $user_data['lastname'] = htmlspecialchars($lastname);
+            $user_data['username'] = htmlspecialchars($_POST['username']);
+            $user_data['email'] = htmlspecialchars($_POST['email']);
+            $user_data['password'] = htmlspecialchars($_POST['password']);
+
+            if ($this->registerIsValid($user_data)) {
+                $user_data['password_hash'] = password_hash($user_data['password'], PASSWORD_BCRYPT, ['cost' => 12]);
+                $this->RegisterManagement->addUser($user_data);
+
+                header('Location: /');
+            }
+                        
+        }
+
     }
 
     public function getFolder() {
@@ -35,11 +56,55 @@ class Authentication extends \Controller {
     }
 
     public function users() {
-        $this->loadModel("LoginManagement");
-        $list =  $this->LoginManagement->getAll();
-        foreach($list as $key => $value) {
-            echo "{$key} => {$value} <br/>";
-        }
+        $this->loadModel("RegisterManagement");
+        $list =  $this->RegisterManagement->removeAll();
     }
 
+    private function registerIsValid(array $data) {
+        $valid = TRUE;
+        
+        if ($this->RegisterManagement->usernameIsUse($data['username'])) {
+            echo '<script> alert("Error: Username is already used."); location.href="/register";</script>';
+            return !$valid;
+        } 
+
+        if ($this->RegisterManagement->emailIsUse($data['email'])) {
+            echo '<script> alert("Error: Email address is already used."); location.href="/register";</script>';
+            return !$valid;
+        }
+
+        if(!$this->pwdIsValid($data['password'])) {
+            echo '<script> alert("Error: Password must contain an uppercase letter, a lowercase letter, one number and a special character (min. lenght: 8 characters)."); location.href="/register";</script>';
+            return !$valid;
+        }
+
+        return $valid;
+    }
+
+    private const MIN_LENGHT = 8;
+    private const UPPERCASE = '/[A-Z]/';
+    private const LOWERCASE = '/[a-z]/';
+    private const SPECIAL = '/[!@#$%^&*]/';
+    private function pwdIsValid(string $pwd) {
+        $valid = TRUE;
+    
+        if (strlen($pwd) < Authentication::MIN_LENGHT) {
+            return !$valid;
+        }
+
+        if(preg_match(Authentication::UPPERCASE, $pwd) == 0) {
+            return !$valid;
+        }
+
+        if (preg_match(Authentication::LOWERCASE, $pwd) == 0) {
+            return !$valid;
+        }
+
+        if (preg_match(Authentication::SPECIAL, $pwd) == 0) {
+            return !$valid;
+        }
+
+
+        return $valid;
+    }
 }
