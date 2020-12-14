5ca69bff8320534518d8d721ce8ddfc68bb47c4b
Saibe <sebastien.cuvellier@hotmail.com>
2020-12-14 20:24:52 +0100

    Autoload and refactor if

--- bootstrap.php
+++ bootstrap.php
@@ -3,35 +3,31 @@
 //We take the parameters of the url 
 $params = explode('/', $_SERVER[REQUEST_URI]);
 
-//Autoreload
+//Autoload
 spl_autoload_register(function ($class_name) {
     $class_name = str_replace('\\', '/', $class_name);
-    require($class_name.'.php');
+    require('class/'. $class_name.'.php');
 });
 
-require_once('class/Controller.php');
-require_once('class/Model.php');
 
 //We see if there is a parameter
-if( ($params[1] != "")  ){
-    $controller = ucfirst($params[1]);
-    $action = isset($params[2]) ? $params[2] : 'index';
-    require_once('controllers/'.$controller.'.php');
-    
-    $controller = new $controller();
-   
-    if(method_exists($controller,$action)){
-
-        unset($params[0]);
-        unset($params[1]);
-        unset($params[2]);
-        call_user_func_array([$controller, $action], $params);
-
-    }else{
-        http_response_code(404);
-        echo "La page existe pas :/";
-    }
-}else{
+if(!($params[1] != "")  ){
     header('Location: /authentication/login');
     exit();
+}
+
+$controller = ucfirst($params[1]);
+$action = isset($params[2]) ? $params[2] : 'index';
+require_once('controllers/'.$controller.'.php');
+
+$controller = new $controller();
+
+if(method_exists($controller,$action)){
+    unset($params[0]);
+    unset($params[1]);
+    unset($params[2]);
+    call_user_func_array([$controller, $action], $params);
+}else{
+    http_response_code(404);
+    echo "La page existe pas :/";
 }
